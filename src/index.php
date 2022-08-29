7842f99ba602b82fa4a267ba329bcb544354c822
Demont Hugo <hugodemont62@hotmail.fr>
2022-08-29 10:41:55 +0200

    Base of header and footer

--- src/index.php
+++ src/index.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="css/style.sass">
     <link rel="icon" type="image/x-icon" href="">
     <!-- Primary Meta Tags -->
-    <meta name="title" content="Vinted clone"">
+    <meta name="title" content="Vinted clone>
     <meta name="description" content="Vinted clone, make by a student.">
     <meta name="keywords"
           content="Hugo Demont,demont,demon,Hugo,hugo,ugo,62,MMI,mmi,lens,valhuon,Lens,Valhuon,contact,me,portfolio,19ans,english,français,french,Hugo Demont MMI,Hugo Demont Lens,Hugo Demont Itch,Huggu,youtube,twitch,photoshop,html,HTML,sass,SASS,css,CSS,php,PHP,symfony,design,backend,frontend, vinted clone">
@@ -14,7 +14,7 @@
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://hugodemont.fr/">
-    <meta property="og:title" content="Vinted clone"">
+    <meta property="og:title" content="Vinted clone">
     <meta property="og:description" content="Vinted clone, make by a student.">
     <meta property="og:image" content="https://hugodemont.fr/img/P1022279.JPG">
 
